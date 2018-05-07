e1aae7c2139e1caf2fc41ae9c1269e3c1b60c5b1
Jirles <emily.jirles@gmail.com>
2018-05-07 13:06:47 -0400

    Drafting stubs for #list_competitions, #menu in cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,8 +4,31 @@ module Tracker
 
     def call
       puts "Howdy, wanna see some Kaggle Competitions?"
+      list_competitions
+      #menu
     end
 
+    def list_competitions(number=5, all=nil)
+      # competitions hard-coded for now
+      # number will eventually control the number of entries seen
+      # if all is invoked as true it will list all competitions
+      puts <<-DOC
+        1. TrackML Particle Tracking Challenge
+          - High Energy Physics particle tracking in CERN detectors
+          - $25,000
+          - 105 Teams
+          - Featured
+        2. Avito Demand Prediction Challenge
+          - Predict demand for an online classified ad
+          - $25,000
+          - 559 Teams
+          - Featured
+      DOC
+    end
+
+    def menu
+      
+    end
   end
-  
+
 end
