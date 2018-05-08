22f45843f230b72a674f3d5e197e17a9295e183e
Jirles <emily.jirles@gmail.com>
2018-05-08 15:13:47 -0400

    added HTTParty gem, did not solve infitite scroll scraping problem

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'httparty'
 
 class Tracker::KaggleScraper
 
   def competition_basic_scraper
-    doc = Nokogiri::HTML(open('https://www.kaggle.com/competitions'))
+    page = HTTParty.get('https://www.kaggle.com/competitions')
+    doc = Nokogiri::HTML(page)
     competition_card = doc.css('div.block-link.block-link--bordered')
     number_competitions = doc.css('div.smart-list__content div div.sc-eIHaNI.eKmMgV span span').text
     binding.pry
